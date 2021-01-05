5b5e34ee468fdc93b10f5294a7032b062917a4ce
DialloAbdou <abdoudiallo3826@yahoo.fr>
2021-01-05 18:46:09 +0100

    controle saisie

--- src/containers/Livres/ForumulaireAjout/FormulaireAjout.js
+++ src/containers/Livres/ForumulaireAjout/FormulaireAjout.js
@@ -3,6 +3,12 @@ import Bouton from '../../../components/Boutons/Bouton'
 
 class FormulaireAjout extends Component {
 
+    state = {
+        titreSaisie: "",
+        auteurSaisie: "",
+        nbPagesSaisie: ""
+    }
+
     render() {
         return (
             <>
@@ -12,19 +18,35 @@ class FormulaireAjout extends Component {
                 <form>
                     <div className="form-group">
                         <label for="titre">Titre du Livre</label>
-                        <input type ="text" className="form-control" id="titre"/>
+                        <input type="text"
+                            className="form-control"
+                            id="titre"
+                            value={this.state.titreSaisie}
+                            onChange={(event) => this.setState({ titreSaisie: event.target.value })}
+                        />
                     </div>
                     <div className="form-group">
-                     <label for="auteur">Auteur</label>
-                     <input type="text" className="form-control" id="auteur"/>
-                
+                        <label for="auteur">Auteur</label>
+                        <input type="text"
+                            className="form-control"
+                            id="auteur"
+                            value={this.state.auteurSaisie}
+                            onChange={(event) => this.setState({ auteurSaisie: event.target.value })}
+
+                        />
+
                     </div>
                     <div className="form-group">
-                       <label for="NbPages">Nombre de psage</label>
-                       <input type="text" className="form-control" />
+                        <label for="NbPages">Nombre de pages</label>
+                        <input type="text"
+                            className="form-control"
+                            id="NbPage"
+                            value={this.state.nbPagesSaisie}
+                            onChange={(event) => this.setState({nbPagesSaisi: event.target.value})}
+                        />
                     </div>
                     <Bouton typeCss="btn-primary">Valider</Bouton>
-                    
+
                 </form>
             </>
 
